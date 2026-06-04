6ee60be09ec41ae81bff7383a1ff4f3cee7af10e
Fiza Eman <fizae47@gmail.com>
2026-06-04 23:01:50 +0500

    create user uodate api

--- api/index.js
+++ api/index.js
@@ -3,12 +3,15 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
